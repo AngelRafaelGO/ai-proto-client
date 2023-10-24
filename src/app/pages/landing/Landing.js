55c25e6f13de55cea7913d5a86e07db08b97f7d0
Angel <angelrafael.gonzalez@outlook.fr>
2023-10-24 12:06:26 +0200

    Added txt and style to landing page, changed project font

--- src/app/pages/landing/Landing.js
+++ src/app/pages/landing/Landing.js
@@ -1,8 +1,7 @@
 import React from "react";
-import logo from "../../assets/logo.svg";
-import "./Landing.css";
-import { Box, Button, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { Box, Button, Typography } from "@mui/material";
+import "./Landing.css";
 
 function Landing() {
   const navigate = useNavigate();
@@ -10,10 +9,20 @@ function Landing() {
   return (
     <Box className="aiprop-landing-main-container">
       <Box>
-        <Typography>Welcome</Typography>
-        <Button variant="outlined" onClick={() => navigate("/chat")}>
-          Chat
-        </Button>
+        <Box>
+          <Typography variant="h1">Bienvenue à IASup</Typography>
+          <Typography sx={{ marginTop: "2rem" }} variant="h6">
+            IASup vous aide dans votre recherche de école et de formation
+          </Typography>
+          <Typography variant="h6">
+            Discutez avec notre IA et trouvez votre voie !
+          </Typography>
+        </Box>
+        <Box className="aiprop-landing-chatbox-navigation">
+          <Button variant="outlined" onClick={() => navigate("/chat")}>
+            Chat
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
